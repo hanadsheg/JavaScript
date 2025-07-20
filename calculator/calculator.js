3814f0b4b271134b894fb1f0ad0f7f47bedd1038
hanad <hanadshegowbusiness@gmail.com>
2025-07-20 21:30:58 +0100

    first commit

---
+++ calculator/calculator.js
@@ -0,0 +1,30 @@
+const AC_button = document.getElementById("clear");
+const negate_button = document.getElementById("negate");
+const percent_button = document.getElementById("percent");
+const divide_button = document.getElementById("divide");
+const multiply_button = document.getElementById("multiply");
+const subtract_button = document.getElementById("subtract");
+const add_button = document.getElementById("add");
+const equals_button = document.getElementById("equals");
+const decimal_button = document.getElementById("decimal");
+const nine_button = document.getElementById("nine");
+const eight_button = document.getElementById("eight");
+const seven_button = document.getElementById("seven");
+const six_button = document.getElementById("six");
+const five_button = document.getElementById("five");
+const four_button = document.getElementById("four");
+const three_button = document.getElementById("three");
+const two_button = document.getElementById("two");
+const one_button = document.getElementById("one");
+const nd_button = document.getElementById("nd");
+const zero_button = document.getElementById("zero");
+
+const display = document.getElementById("display");
+
+
+class Calculator {
+  constructor() {
+    this.currentInput = "";
+    this.previousInput = "";
+    this.operation = null;
+  }
